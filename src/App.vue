ef0513c991b47a7c832ba7a043d9ab69be3d62e6
Iago Real <iagovreal@gmail.com>
2024-06-21 16:02:50 -0300

    build: structure initial of the project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+
+}
+</script>
+
+<template>
+  
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+  
+
+  <RouterView />
+</template>
+
+<style >
+
+</style>
---
+++ src/views/Home/HomeView.vue
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+</script>
+
+<template>
+<h1>This is Home</h1>
+</template>
